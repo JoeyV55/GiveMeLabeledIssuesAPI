f395939b83f66acf06b0ae32055124153625d024
DovahCraft <josephvargovich@gmail.com>
2022-10-25 13:42:35 -0700

    Created JabRef and PowerToys issue models

--- GiveMeLabeledIssues/BERT/models.py
+++ GiveMeLabeledIssues/BERT/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class JabRefIssue(models.Model):
+    #Number
+    issueNumber = models.IntegerField(primary_key = True, db_column='IssueNumber')
+
+    #Labels 
+    Util = models.BooleanField()
+    NLP = models.BooleanField()
+    APM = models.BooleanField()
+    Network = models.BooleanField()
+    DB = models.BooleanField()
+    Interpreter = models.BooleanField()
+    Logging = models.BooleanField()
+    Data_Structure = models.BooleanField(db_column = 'Data.Structure')
+    i18n = models.BooleanField()
+    DevOps = models.BooleanField()
+    Logic = models.BooleanField()
+    Microservices = models.BooleanField()
+    Test = models.BooleanField()
+    Search = models.BooleanField()
+    IO = models.BooleanField()
+    UI = models.BooleanField()
+    Parser = models.BooleanField()
+    Security = models.BooleanField()
+    App = models.BooleanField()
+
+
+class PowerToysIssue(models.Model):
+    #Number
+    issueNumber = models.IntegerField(primary_key = True, db_column='IssueNumber')
+
+    #Labels 
+    APM = models.BooleanField()
+    Interpreter = models.BooleanField()
+    Logging = models.BooleanField()
+    Data_Structure = models.BooleanField(db_column = 'Data.Structure')
+    i18n = models.BooleanField()
+    Setup = models.BooleanField()
+    Logic = models.BooleanField()
+    Microservices = models.BooleanField()
+    Test = models.BooleanField()
+    Search = models.BooleanField()
+    UI = models.BooleanField()
+    Parser = models.BooleanField()
+    App = models.BooleanField()
